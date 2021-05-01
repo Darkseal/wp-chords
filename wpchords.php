6029ffef6d742704a86cd1063dbc69d4ecbeb811
Dark <games@myst.it>
2021-05-02 00:54:20 +0200

    Update wpchords.php

--- wpchords.php
+++ wpchords.php
@@ -17,13 +17,13 @@ it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
 any later version.
 
-WP Chords is distributed in the hope that it will be useful,
+WP Chords Fork is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with WP Chords. If not, see emreplt.com.
+along with WP Chords Fork. If not, see the plugin's GitHub page.
 */
 
 require plugin_dir_path(__FILE__).'init.php';
